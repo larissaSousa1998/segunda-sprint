b78470d7f06e55bfc794a07e849a5cac70be02d6
lucasmino <lucas.sa@bandtec.com.br>
2020-05-01 23:48:28 -0300

    Cadastro/login

--- site-institucional/js/modal.js
+++ site-institucional/js/modal.js
@@ -28,5 +28,63 @@ function ant(){
     fotomodal.style.transition = '0.5s';
 }
 
+function removerlogin(){
+    document.getElementById('remover-cadastro').style.display="none";
+    document.getElementById('altera-login').style.display="block";
+    document.getElementById('div-top').style.display="none";
+    document.getElementById('div-top2').style.display="block";
+}
 
+
+function voltarlogin(){
+    document.getElementById('remover-cadastro').style.display="block";
+    document.getElementById('altera-login').style.display="none";
+    document.getElementById('div-top').style.display="block";
+    document.getElementById('div-top2').style.display="none";
+}
+
+var btnlogar = document.querySelector('#id-btn');
+var btncadastrar = document.querySelector('#id-cad'); 
+
+var email='';
+var senha='';
+
+function cadastrar(e){
+    e.preventDefault();
+
+    if(senhacad.value == confirmacad.value){
+        email = document.getElementById('emailcad').value;
+        senha= document.getElementById('senhacad').value;
+        alert("Você está cadastrado!");
+        voltarlogin();
+    } else{
+        alert("As senhas não coincidem");
+        document.getElementById('senhacad').value="";
+        document.getElementById('confirmacad').value="";
+    }
+    
+
+}
+
+
+function log(e){
+    e.preventDefault();
+    var auxlogin=document.getElementById('emaillogin').value;
+    var auxsenha=document.getElementById('senhalogin').value;
+    if(email==auxlogin && senha==auxsenha){
+        window.location.href="google.com";
+
+    } else {
+        alert("Email ou senha estão incorretos");
+    }
+
+
+}
+
+ btnlogar.addEventListener("click",log);
+btncadastrar.addEventListener("click",cadastrar);
+
+
+
+ 
 
